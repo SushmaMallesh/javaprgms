e8c37bd4ff8f6f485242c9047e0270d7b86f46ea
sushmamallesh <aishusushma57@gmail.com>
2021-07-29 13:03:12 +0530

    push decrement operator program

---
+++ DecrementOperator.java
@@ -0,0 +1,13 @@
+class DecrementOperator
+{
+public static void main(String args[])
+{
+int x=1;
+int y=10;
+int z=x--;
+z--;
+System.out.println(x+y);
+System.out.println(--z+y);
+System.out.println((y--)-(--x));
+}
+}
