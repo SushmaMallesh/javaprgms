39db6863362ba30ee4a931ac05a8f8b1412cff88
sushmamallesh <aishusushma57@gmail.com>
2021-07-29 12:50:13 +0530

    push relationalOperators program

---
+++ RelationalOperators.java
@@ -0,0 +1,14 @@
+class RelationalOperators
+{
+public static void main(String[] args)
+{
+int p=1000;
+int q=2000;
+System.out.println(p==q);
+System.out.println(p!=q);
+System.out.println(p>=q);
+System.out.println(p<=q);
+System.out.println(p>q);
+System.out.println(p<q);
+}
+}
