de9c1d0cb22385b95226873461e47685b4630488
sushmamallesh <aishusushma57@gmail.com>
2021-08-02 08:37:43 +0530

    push nestedforloop11 program

---
+++ NestedForLoop11.java
@@ -0,0 +1,18 @@
+class NestedForLoop11
+{
+public static void main(String args[])
+{
+for(int i=1;i<=1;i++)
+{
+for(int j=1;j<=4;j++)
+{
+System.out.print(" " +j);
+}
+}
+System.out.println();
+for(int k=5;k<=8;k++)
+{
+System.out.print(" "+k);
+}
+}
+}
