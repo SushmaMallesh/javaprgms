02d9b6c9fbc2ce57746396819e2bebda811df1d6
sushmamallesh <aishusushma57@gmail.com>
2021-08-02 08:39:18 +0530

    push nestedforlooop program

---
+++ NestedForLooop.java
@@ -0,0 +1,18 @@
+class NestedForLooop
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=4;i++);
+{
+for(int j=1;j<=8;j++)
+{
+if(j%2==0)
+{
+System.out.println(" "+j);
+}
+}
+System.out.println();
+}
+}
+}
+
